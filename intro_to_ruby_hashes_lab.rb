b7713010981f521c522c8984d4277d942f44ea68
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 04:14:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	monopoly = {
+	  :rent_in_dollars => {
+	    :one_piece_owned => 25
+	  }
+	}
 
 end
 
