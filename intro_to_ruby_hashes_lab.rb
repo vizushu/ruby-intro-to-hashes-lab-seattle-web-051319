70f7508efba0d13aeef57b590cb3acaba89cd03a
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 04:29:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,58 +1,53 @@
-# def new_hash
-#   new_hash = {
-#   }
-# end
-
-# def actor
-# 	actor = {
-# 		:name => "Dwayne The Rock Johnson"
-# 	}
-# end	end
-
-# def monopoly
-# 	monopoly = {
-# 	  :railroads => {}
-#   }
-# end
-
-# def monopoly_with_second_tier
-# 	monopoly = {
-# 	  :railroads => {
-# 	    :pieces => 4,
-# 	    :names => {},
-# 	    :rent_in_dollars => {}
-# 	  }
-#   }
-
-# end
-
-# def monopoly_with_third_tier
-# 	monopoly = {
-# 	  :rent_in_dollars => {
-# 	    :one_piece_owned => 25,
-# 	    :two_piece_owned => 50,
-# 	    :three_piece_owned => 100,
-# 	    :four_piece_owned => 200
-# 	  }
-# 	  :names => {
-# 	    :reading_railroad => {},
-# 	    :pennsylvania_railroad => {},
-# 	    :b_and_o_railroad => {},
-# 	    :shortline => {},
-# 	  }
-# 	}
-
-# end
-
-# def monopoly_with_fourth_tier
-
-
-# end
-
-
 def new_hash
   new_hash = {}
 end
+
+def actor
+	actor = {
+		:name => "Dwayne The Rock Johnson"
+	}
+end	end
+
+def monopoly
+	monopoly = {
+	  :railroads => {}
+  }
+end
+
+def monopoly_with_second_tier
+	monopoly = {
+	  :railroads => {
+	    :pieces => 4,
+	    :names => {},
+	    :rent_in_dollars => {}
+	  }
+  }
+
+end
+
+def monopoly_with_third_tier
+	monopoly = {
+	  :rent_in_dollars => {
+	    :one_piece_owned => 25,
+	    :two_piece_owned => 50,
+	    :three_piece_owned => 100,
+	    :four_piece_owned => 200
+	  }
+	  :names => {
+	    :reading_railroad => {},
+	    :pennsylvania_railroad => {},
+	    :b_and_o_railroad => {},
+	    :shortline => {},
+	  }
+	}
+
+end
+
+def monopoly_with_fourth_tier
+
+
+end
+
 
 def actor
  	actor = {
