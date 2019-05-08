e82dcffe57b2afc2875a5df7d596010368633644
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 04:39:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,32 +25,6 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {
-	  :rent_in_dollars => {
-	    :one_piece_owned => 25,
-	    :two_piece_owned => 50,
-	    :three_piece_owned => 100,
-	    :four_piece_owned => 200
-	  }
-	  :names => {
-	    :reading_railroad => {},
-	    :pennsylvania_railroad => {},
-	    :b_and_o_railroad => {},
-	    :shortline => {},
-	  }
-	}
-
-end
-
-def monopoly_with_fourth_tier
-
-
-end
-
-
-
-
- def monopoly_with_third_tier
  	monopoly = {
  	  :railroads => {
   		:pieces => 4,
