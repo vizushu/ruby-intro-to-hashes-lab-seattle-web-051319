11f730485f4db3073bc38c6584eae1da0b99c98a
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 04:19:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,13 +29,22 @@ end
 def monopoly_with_third_tier
 	monopoly = {
 	  :rent_in_dollars => {
-	    :one_piece_owned => 25
+	    :one_piece_owned => 25,
+	    :two_piece_owned => 50,
+	    :three_piece_owned => 100,
+	    :four_piece_owned => 200
+	  }
+	  :names => {
+	    :reading_railroad => {},
+	    :pennsylvania_railroad => {},
+	    :b_and_o_railroad => {},
+	    :shortline => {},
 	  }
 	}
 
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+
 
 end
